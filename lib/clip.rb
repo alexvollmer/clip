e7057a7d5e12a056cc885e146a2ca4a4f0678c27
Alex Vollmer <alex.vollmer@gmail.com>
2008-04-03 21:14:32 -0700

    Refactored the internals for clarity and to support flags.

--- lib/clip.rb
+++ lib/clip.rb
@@ -3,44 +3,107 @@
 module Clip
   VERSION = "0.0.1"
 
-  # Signals a command-line option that is not recognized by the +CliParser+
-  # implementation.
-  class UnrecognizedOption < StandardError
-  end
-
-  # Signals that a required command-line argument was not given when options were parsed
-  class MissingArgument < StandardError
-  end
-
   # The base class for command-line parser. Specific parsers should extend this class
   # and provide simple attribute methods matching the options to be parsed
   class Parser
-    def self.opt(name, options={})
-      attr_accessor name.to_sym
-      if options[:short]
-        alias_method options[:short].to_sym, name.to_sym
-        alias_method "#{options[:short]}=".to_sym, "#{name}=".to_sym
-      end
+    class << self
+      ##
+      # Declare an optional parameter for your parser. This creates an accessor
+      # method matching the <tt>name</tt> parameter.
+      # === options
+      # Valid options include:
+      # * <tt>short</tt>: a single-character version of your parameter
+      # * <tt>desc</tt>: a helpful description (used for printing usage)
+      # * <tt>default</tt>: a default value to provide if one is not given
+      def opt(name, options={})
+        attr_accessor name.to_sym
+        if options[:short]
+          alias_method options[:short].to_sym, name.to_sym
+          alias_method "#{options[:short]}=".to_sym, "#{name}=".to_sym
+        end
     
-      self.options << Option.new(name, options[:short], options[:desc], options[:default], options[:required])
+        self.options << Option.new(name, options)
+      end
+
+      ##
+      # Declare a required parameter for your parser. If this parameter
+      # is not provided in the parsed content, the parser instance
+      # will be invalid (i.e. where +valid?+ return <tt>false</tt>).
+      def req(name, options={})
+        attr_accessor name.to_sym
+        if options[:short]
+          alias_method options[:short].to_sym, name.to_sym
+          alias_method "#{options[:short]}=".to_sym, "#{name}=".to_sym
+        end
+
+        self.options << Option.new(name, options.merge({ :required => true }))
+      end
+
+      ##
+      # Declare a parameter as a simple binary flag. This declaration
+      # will create a "question" method matching the given <tt>name</tt>.
+      # For example, declaring with the name of 'verbose' will create a 
+      # method your parser called <tt>verbose?</tt>.
+      # === options
+      # Valid options are:
+      # * <tt>short</tt>: A single-character flag accepted for parsing
+      # * <tt>desc</tt>: Descriptive text for the flag
+      def flag(name, options={})
+        class_eval <<-EOF
+          def flag_#{name}
+            @#{name} = true
+          end
+
+          def #{name}?
+            return @#{name}
+          end
+        EOF
+
+        if options[:short]
+          class_eval <<-EOF
+            alias_method :#{options[:short]}?, :#{name}?
+            alias_method :flag_#{options[:short]}, :flag_#{name}
+          EOF
+        end
+
+        self.options << Option.new(name, options.merge({ :required => false, :default => nil }))
+      end
     end
     
+    def initialize
+      @errors = {}
+    end
+
+    ##
+    # Parse the given <tt>args</tt> and set the corresponding instance
+    # fields to the given values. If any errors occurred during parsing
+    # you can get them from the <tt>Hash</tt> returned by the +errors+ method.
     def parse(args)
       @valid = true
       args = args.split(/\s+/) unless args.kind_of?(Array)
-      flag, value = nil, nil
+      param, value = nil, nil
     
       args.each do |token|
         case token
         when /^-(-)?\w/
-          flag = token.sub(/^-(-)?/, '').sub('-', '_')
+          param = token.sub(/^-(-)?/, '').sub('-', '_')
           value = nil
         else
           value = token
         end
       
-        raise UnrecognizedOption.new("Unknown option '#{flag}'") unless self.respond_to?(flag.to_sym)
-        self.send("#{flag}=".to_sym, value.nil? ? true : value)
+        unless self.respond_to?(param.to_sym) or self.respond_to?("#{param}?".to_sym)
+          @errors[param.to_sym] = "Unrecoginzed parameter"
+          @valid = false
+          next
+        end
+
+        if self.respond_to?("#{param}=")
+          self.send("#{param}=".to_sym, value.nil? ? true : value)
+        else
+          # FIXME: what happens when somebody sends us a value?
+          self.send("flag_#{param}")
+        end
       end
     
       self.class.options.each do |option|
@@ -51,15 +114,27 @@ module Clip
     
       self.class.options.find_all { |e| e.required? }.each do |required|
         unless self.send(required.long.to_sym)
-          raise MissingArgument.new("Missing required arg, '#{required.long}'")
+          @valid = false
+          @errors[required.long.to_sym] = "Missing required parameter"
+          # raise MissingArgument.new("Missing required arg, '#{required.long}'")
         end
       end
-    rescue UnrecognizedOption, MissingArgument
+    rescue UnrecognizedOption
       @valid = false
     end
 
+    ##
+    # Indicates whether or not the parsing process was valid.
+    # It only makes sense to call this _after_ calling +parse+.
     def valid?
       @valid
+    end
+
+    ##
+    # Returns a <tt>Hash</tt> of errors (by parameter) of any errors
+    # encountered during parsing.
+    def errors
+      @errors
     end
   
     def help(*args)
@@ -74,7 +149,8 @@ module Clip
         out << "#{option.usage}\n"
       end
     end
-  
+
+    private
     def self.inherited(sub)
       sub.class_eval <<-EOF
         def self.options
@@ -84,11 +160,16 @@ module Clip
     end    
   end
 
+  private
   class Option
     attr_accessor :long, :short, :description, :default, :required
-  
-    def initialize(long, short, description, default, required)
-      @long, @short, @description, @default, @required = long, short, description, default, required
+
+    def initialize(name, options)
+      @long = name
+      @short = options[:short]
+      @description = options[:desc]
+      @default = options[:default]
+      @required = options[:required]
     end
 
     def required?
@@ -109,4 +190,5 @@ module Clip
       out.string
     end
   end
+
 end
--- spec/clip_spec.rb
+++ spec/clip_spec.rb
@@ -26,6 +26,7 @@ describe "When long command-line parameters are parsed" do
     @parser.parse %w(--verbose --debug)
     @parser.should be_in_debug
     @parser.should be_verbose
+    @parser.should be_valid
   end
   
   it "should set fields for flags with the given values" do
@@ -64,10 +65,12 @@ describe "When short (single-letter) command-line parse are parsed" do
   it "should set fields to true when no arguments are given" do
     @parser.parse("-v")
     @parser.should be_verbose
+    @parser.should be_valid
   end
   
   it "should set fields for short options" do
     @parser.parse("-h localhost -p 8080")
+    @parser.should be_valid
     @parser.host.should eql("localhost")
     @parser.port.should eql("8080")
   end
@@ -76,8 +79,8 @@ end
 describe "When usage for the parser is requested" do
   before do
     class UsageParser < Clip::Parser
-      opt "host", :short => "h", :desc => "The hostname", :default => "localhost"
-      opt "port", :short => "p", :desc => "The port number", :required => true
+      opt :host, :short => "h", :desc => "The hostname", :default => "localhost"
+      req :port, :short => "p", :desc => "The port number"
     end
     @parser = UsageParser.new
   end
@@ -97,7 +100,7 @@ describe "When parameters are marked as required" do
   before do
     class RequiredParser < Clip::Parser
       opt "host"
-      opt "port", :required => true
+      req "port"
     end
     @parser = RequiredParser.new
   end
@@ -125,11 +128,51 @@ describe "When parameters are marked with defaults" do
   
   it "should use parsed parameter values" do
     @parser.parse %w(--host foobar)
+    @parser.should be_valid
     @parser.host.should eql("foobar")
   end
   
   it "should provide default parameter values when none are parsed" do
     @parser.parse []
+    @parser.should be_valid
     @parser.host.should eql("localhost")
+  end
+end
+
+describe "When specifying flags" do
+  before(:each) do
+    class FlagParser < Clip::Parser
+      flag :verbose, :short => 'v'
+      flag :debug
+    end
+    @parser = FlagParser.new
+  end
+
+  it "should create accessor methods" do
+    @parser.respond_to?(:flag_verbose).should == true
+    @parser.respond_to?(:verbose?).should == true
+    @parser.respond_to?(:flag_debug).should == true
+    @parser.respond_to?(:debug?).should == true
+  end
+
+  it "should indicate given flags" do
+    @parser.parse %w(--debug --verbose)
+    @parser.should be_valid
+    @parser.should be_debug
+    @parser.should be_verbose
+  end
+
+  it "should indicate missing flags" do
+    @parser.parse []
+    @parser.should_not be_debug
+    @parser.should_not be_verbose
+    @parser.should be_valid
+  end
+
+  it "should support short-name versions" do
+    @parser.parse %w(-v)
+    puts @parser.errors
+    @parser.errors.should be_empty
+    @parser.should be_valid
   end
 end
